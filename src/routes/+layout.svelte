74148a6174cf9be06d4f16c82da9c214bafd78a5
vmosiichuk-dev <vmosiichuk.dev@gmail.com>
2025-11-22 08:10:00 +0100

    Add layout header + start implementing main screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,66 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import { ModeToggle } from '$ui/mode-toggle';
+	import { Button } from '$ui/button';
+	import { Menu } from '@lucide/svelte';
+	import * as Avatar from '$ui/avatar';
+	import * as Sheet from '$ui/sheet';
 	import './layout.css';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
 <div class="contain-layout">
-	<ModeWatcher />
-	<ModeToggle />
+	<header class="flex justify-between">
+		<Sheet.Root>
+			<Sheet.Trigger>
+				<Button size="icon-sm">
+					<Menu />
+				</Button>
+			</Sheet.Trigger>
+
+			<Sheet.Content side="left">
+				<Sheet.Header>
+					<Sheet.Title>Edit profile</Sheet.Title>
+
+					<Sheet.Description>
+						Make changes to your profile here. Click save when you're done.
+					</Sheet.Description>
+				</Sheet.Header>
+
+				<ModeToggle />
+
+				<!-- <Sheet.Footer></Sheet.Footer> -->
+			</Sheet.Content>
+		</Sheet.Root>
+
+
+		<Sheet.Root>
+			<Sheet.Trigger>
+				<Avatar.Root>
+					<Avatar.Image src="https://github.com/leerob.png" alt="@leerob" />
+
+					<Avatar.Fallback>LR</Avatar.Fallback>
+				</Avatar.Root>
+			</Sheet.Trigger>
+
+			<Sheet.Content side="right">
+				<Sheet.Header>
+					<Sheet.Title>Edit profile</Sheet.Title>
+
+					<Sheet.Description>
+						Make changes to your profile here. Click save when you're done.
+					</Sheet.Description>
+				</Sheet.Header>
+
+				<ModeToggle />
+
+				<!-- <Sheet.Footer></Sheet.Footer> -->
+			</Sheet.Content>
+		</Sheet.Root>
+	</header>
+
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
-	import { ROUTES } from '$constants';
 	import { getBrowserTabTitle } from '$utils';
+	import { ROUTES } from '$constants';
 
-	const title = getBrowserTabTitle(ROUTES.HOMEPAGE.title);
+	let user = $state();
+	let tasks = $state();
 </script>
 
 <svelte:head>
-	<title>{title}</title>
+	<title>{getBrowserTabTitle(ROUTES.HOMEPAGE.title)}</title>
 </svelte:head>
 
-<h1>{ROUTES.HOMEPAGE.title}</h1>
+<p class="text-muted-foreground text-sm">
+	Good morning {user ? `, ${user.name}` : ''}
+</p>
+
+<h1 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+	{#if tasks?.today}
+		You have {tasks.today.length} tasks this month
+	{:else if tasks?.today.length === 0 && tasks?.done.length > 0}
+		You have completed {tasks} tasks this month
+	{:else}
+		Create your task
+	{/if}
+</h1>
