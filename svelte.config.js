a2a83839b72b89ea3791a51dfb27418b6f684150
vmosiichuk-dev <vmosiichuk.dev@gmail.com>
2025-11-23 12:15:36 +0100

    Change adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess({ script: true }),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
 		alias: {
 			$assets: 'src/lib/assets',
 			$components: 'src/lib/components',
